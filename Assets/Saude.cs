9139b32a3fc1a405ad06d58e74fecf2516b3ca40
Arthur <arthur.p31@aluno.ifsc.edu.br>
2025-07-03 11:07:49 -0300

    Added health relaed scripts

---
+++ Assets/Saude.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Saude : MonoBehaviour
+{
+
+    public bool morto;
+    public int saude;
+    private Animator animator;
+    public float deathTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        morto = false;
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+    }
+
+    public void dano(int x)
+    {
+        Debug.Log("Entrei dano");
+        saude -= x;
+        if (saude <= 0)
+        {
+            morto = true;
+            animator.SetTrigger("Morte");
+            if (gameObject.tag == "Player")
+            {  // Só reicicia a fase se quem morreu foi o jogador.
+                StartCoroutine(morre());
+            }
+        }
+    }
+
+    public void danoMax()
+    {
+        saude = 0;
+        morto = true;
+        animator.SetTrigger("Morte");
+        if (gameObject.tag == "Player")
+        {  // Só reicicia a fase se quem morreu foi o jogador.
+            StartCoroutine(morre());
+        }
+    }
+
+    IEnumerator morre()
+    {
+        yield return new WaitForSeconds(deathTime);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
